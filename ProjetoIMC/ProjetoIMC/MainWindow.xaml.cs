84da4e4342777c001123ab80e10909b6eb4732a1
PedroLucasRosadoAndrade <126489915+PedroLucasRosadoAndrade@users.noreply.github.com>
2023-04-17 17:30:17 -0400

    adicionei o arquivo

---
+++ ProjetoIMC/ProjetoIMC/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ProjetoIMC.RegrasDeNegocio;
+
+namespace ProjetoIMC
+{
+    /// <summary>
+    /// Interação lógica para MainWindow.xam
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btCalcular_Click(object sender, RoutedEventArgs e)
+        {
+            CalcularIMC objeto1 = new CalcularIMC();
+            objeto1.Altura = Convert.ToDouble(edAltura.Text);
+            objeto1.Peso = Convert.ToDouble(edPeso.Text);
+            objeto1.ClassCalcularIMC();//prosesamento
+            edIndice.Text = objeto1.Indice.ToString("F2");
+            edSituacao.Text = objeto1.Classificassao(); //+"," + "seu indice é: " + objeto1.Classificassao();
+        }
+
+        private void btLimpar_Click(object sender, RoutedEventArgs e)
+        {
+            edAltura.Clear();
+            edPeso.Clear();
+            edIndice.Clear();
+            edSituacao.Clear();
+            edAltura.Focus();
+        }
+
+        private void btSair_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult rep;
+            rep = MessageBox.Show("Deseja mesmo fechar?", "Calculo de IMC", MessageBoxButton.YesNo, MessageBoxImage.Question);
+            if (rep == MessageBoxResult.Yes)
+            {
+                this.Close();
+            }
+        }
+    }
+}
---
+++ ProjetoIMC/ProjetoIMC/RegrasDeNegocio/CalcularIMC.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoIMC.RegrasDeNegocio
+{
+    internal class CalcularIMC
+    {
+        public double Peso { get; set; }
+        public double Altura { get; set; } //em metros
+        public double Indice { get; set; }
+
+        public void ClassCalcularIMC()
+        {
+            Indice = Peso / (Altura * Altura);
+
+        }
+        public string Classificassao()
+        {
+            string situacao = "";
+            /*if (Indice < 18.5) //situacao = "Magreza";
+            {
+                situacao = "Magreza";
+            }
+            if (Indice > 18.5 && Indice < 24.9)//situacao = "Normal";//Exemplo do profesor
+            {
+                situacao = "Normal";
+            }*/
+            if (Indice > 40) //situacao = "Obesidade";
+            {
+                situacao = "Obesidade de Grau III";
+            }
+            else if (Indice > 35)
+            {
+                situacao = "Obesidade de Grau II";
+            }
+            else if (Indice > 30)
+            {
+                situacao = "Obesidade de Grau I";
+            }
+            else if (Indice > 25)
+            {
+                situacao = "Sobrepeso";
+            }
+            else if (Indice >= 18.5)
+            {
+                situacao = "Normal";
+            }
+            else
+            {
+                situacao = "Magreza";
+            }
+            return situacao;
+        }
+
+
+    }
+
+}
